9401a702d52f9366a6a2cfa55a80e6eed63e4d98
= <=>
2025-11-09 14:54:51 +0100

    update debug script

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "=== DEBUG ==="
 echo "DATABASE_URL=$DATABASE_URL"
+echo "ADMIN_CODE=$ADMIN_CODE"
+echo "FRONTEND_URL=$FRONTEND_URL"
+echo "PORT=$PORT"
 ls -la /app
 echo "=== STARTING BACKEND ==="
 ./wedding_list_backend
